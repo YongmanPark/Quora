27b49fcd9e5f1e77c46e24a59997868914e20c96
yongman park <yongman514@gmail.com>
2022-06-04 19:27:40 +0900

    quora clone coding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Quora from './Quora.js';
 
 function App() {
   return (
     <div className="App">
       <h1>test</h1>
+    <Quora/>
     </div>
   );
 }
---
+++ src/Quora.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Quora.css';
+import Navbar from './Navbar.js';
+
+function Quora(props) {
+    return (
+        <div>
+            quora js
+        <Navbar/>
+        </div>
+    );
+}
+
+export default Quora;
