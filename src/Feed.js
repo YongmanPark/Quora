fbb7dffb608d622999c7d7a8b39a8d32bbd9ca3f
yongman park <yongman514@gmail.com>
2022-06-06 23:20:17 +0900

    quora clone coding

---
+++ src/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Feed.css';
+import QuoraBox from './QuoraBox';
+function Feed() {
+    return (
+        <div className='feed'>
+           <QuoraBox/> 
+        </div>
+    );
+}
+
+export default Feed;
--- src/Quora.js
+++ src/Quora.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Quora.css';
 import Navbar from './Navbar.js';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
-function Quora(props) {
+function Quora() {
     return (
         <div>
         <Navbar/>
         <div className='quora_content'>
             <Sidebar/>
+            <Feed/>
         </div>
         </div>
     );
